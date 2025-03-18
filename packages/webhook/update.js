0e5318299abb652e0b0a423dd37189c9257aa37e
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2025-03-18 18:01:19 +0200

    Add project files

---
+++ packages/webhook/update.js
@@ -0,0 +1,61 @@
+const crypto = require('crypto')
+
+async function main(args) {
+  // Parse the user's input (Sanapyramidi results, hopefully)
+  const inlineQuery = args.inline_query
+  const inputText = inlineQuery.query
+
+  // Only keep lines without any letters
+  const noAlphabetsRegex = /^[^a-zA-Z]*$/
+  const outputText = inputText
+    .split('\n')
+    .filter(line => noAlphabetsRegex.test(line))
+    .join('\n')
+    .trim()
+
+  // Send the results as a response to the webhook invocation
+  return {
+    statusCode: 200,
+    body: {
+      method: 'answerInlineQuery',
+      inline_query_id: inlineQuery.id ?? null,
+      // Maximum caching time in seconds:
+      cache_time: 60*60*24, // 1 day
+      // Render two result "articles" for the user
+      results: [{ withButtons: true }, { withButtons: false }]
+        .map(result => ({
+          type: 'article',
+          id: crypto.randomUUID(),
+          title: result.withButtons ? 'Siivottu versio' : 'Ilman nappeja',
+          description: outputText,
+          // Content of the message that will be sent after clicking the article
+          input_message_content: { 'message_text': outputText },
+          // These buttons will be shown in the message which is sent after the user clicks the article.
+          // The first article produces buttons, the second does not (undefined = `reply_markup` is not included).
+          reply_markup: result.withButtons ? {
+            inline_keyboard: [
+              [{
+                text: 'Pelaa',
+                url: 'https://yle.fi/a/74-20131998',
+              }],
+              [{
+                text: 'Jaa oma tulos',
+                switch_inline_query_current_chat: '',
+              }]
+            ]
+          } : undefined,
+        })),
+    },
+  }
+}
+
+// Function for debugging since console.log cannot be read
+async function log(args) {
+  await fetch('<insert debugging server url here>', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(args)
+  })
+}
